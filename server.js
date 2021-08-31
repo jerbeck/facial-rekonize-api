e5a8f71afc952d586273f3a383c7d84b8dc4da76
jeremy <jeremy@jeremybeck.io>
2021-08-31 11:40:51 -0700

    Added databse support

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
+const knex = require('knex')
+
+const db = knex({
+	client: 'pg',
+	connection: {
+		host: '127.0.0.1',
+		user: 'jbeck',
+		password: '',
+		database:'facial-reko'
+	}
+});
 
 const saltRounds = 10;
 const app = express();
@@ -8,38 +19,30 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const database = {
-	users: [
-		{
-			id: '123',
-			name: 'John',
-			email: 'john@gmail.com',
-			password: 'cookies',
-			entries: 0,
-			joined: new Date()
-		},
-		{
-			id: '124',
-			name: 'Sally',
-			email: 'sally@gmail.com',
-			password: 'bananas',
-			entries: 0,
-			joined: new Date()
-		}
-	]
-}
-
 app.get('/', (req, res) => {
-	res.send(database.users);
+	res.send('success');
 });
 
 app.post('/signin', (req, res) => {
-	if (req.body.email === database.users[0].email && 
-			req.body.password === database.users[0].password) {
-		res.json(database.users[0]);
-	} else {
-		res.status(400).json('error logging in');
-	}
+	db.select('email', 'hash').from('login')
+		.where('email', '=', req.body.email)
+		.then(data => {
+			bcrypt.compare(req.body.password, data[0].hash)
+				.then(result => {
+					if (result) {
+						return db.select('*').from('users')
+							.where('email', '=', req.body.email)
+							.then(user => {
+								res.json(user[0])
+							})
+							.catch(err => res.json('Unable to get user data'));
+					} else {
+						res.status(400).json('Login Failed')
+					}
+				})
+				.catch(err =>	res.status(400).json('Login Failed'));
+		})
+		.catch(err => res.json('Wrong Credentials'));
 });
 
 app.post('/register', (req, res) => {
@@ -49,47 +52,54 @@ app.post('/register', (req, res) => {
 			console.log(err);
 			res.send("An error occured: ", err);
 		} else {
-			database.users.push({
-				id: '125',
-				name: name,
-				email: email,
-				password: hash,
-				entries: 0,
-				joined: new Date()
-			});
+			db.transaction(trx => {
+				trx.insert({
+					hash: hash,
+					email: email
+				})
+				.into('login')
+				.returning('*')
+				.then(loginEmail => {
+					return trx('users')
+						.returning('*')
+						.insert({
+								email: loginEmail[0].email,
+								name: name,
+								joined: new Date()
+						})
+						.then(user => {
+							res.json(user[[0]]);
+						})
+				})
+				.then(trx.commit)
+				.catch(trx.rollback);
+			})
+			.catch(err => res.status('400').json("Unable to register"));
 		}
-		res.json(database.users[database.users.length - 1]);
 	});
 });
 
-
 app.get('/profile/:id', (req, res) => {
 	const { id } = req.params;
-	let found = false;
-	database.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			return res.json(user);
-		} 
-	});
-	if (!found) {
-		res.status(404).json('no such user');
-	}
+	db.select().from('users').where({id})
+		.then(user => {
+			if (user.length) {
+				res.json(user[0]);
+			} else {
+				res.status(400).json('Not Found')
+			}
+		})
+		.catch(err => res.status(400).json('Error getting user profile'));
 });
 
 app.put('/image', (req, res) => {
 	const { id } = req.body;
 	let found = false;
-	database.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			user.entries++;
-			return res.json(user.entries);
-		} 
-	});
-	if (!found) {
-		res.status(404).json('no such user');
-	}
+	db('users').where('id', '=', id)
+		.increment('entries', 1)
+		.returning('entries')
+		.then(entries => res.json(entries[0]))
+		.catch(err => res.status(400).json('Could not get entries'));	
 });
 
 app.listen(3000, () => {
